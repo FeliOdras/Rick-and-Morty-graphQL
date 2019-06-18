6efdeef4731d5525ea11f3008e34c4b0e26b37f4
FeliOdras <ariane.feldmann@odras.de>
2019-06-18 15:00:18 +0200

    Added further info

--- src/components/SingleCharacter.js
+++ src/components/SingleCharacter.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
+import "../style/style.scss";
 
 const SingleCharacterQuery = gql`
   query($character: String!, $page: Int!) {
@@ -14,6 +15,14 @@ const SingleCharacterQuery = gql`
       results {
         name
         id
+        image
+        species
+        type
+        status
+        gender
+        origin {
+          name
+        }
       }
     }
   }
@@ -24,11 +33,15 @@ const SingleCharacter = info => {
   const [page, setPage] = useState(1);
   return (
     <>
-      <input
-        type="text"
-        value={character}
-        onChange={e => setCharacter(e.target.value)}
-      />
+      <header className="header">
+        <h1>Rick & Morty Character Base</h1>
+        <input
+          type="text"
+          value={character}
+          placeholder="Type character name"
+          onChange={e => setCharacter(e.target.value)}
+        />
+      </header>
       <Query variables={{ page, character }} query={SingleCharacterQuery}>
         {({
           loading,
@@ -46,7 +59,56 @@ const SingleCharacter = info => {
             <>
               {info.count > 0 && info.count}
               {results
-                ? results.map(({ name, id }) => <p key={id}>{name}</p>)
+                ? results.map(
+                    ({
+                      name,
+                      id,
+                      image,
+                      species,
+                      status,
+                      gender,
+                      type,
+                      origin
+                    }) => (
+                      <div className="character-details" key={id}>
+                        <h3>
+                          {name}
+                          <span className="subtitle">
+                            {" "}
+                            ({species}
+                            {type !== "" ? ", " : ""}
+                            {type})
+                          </span>
+                        </h3>
+                        <img
+                          src={image}
+                          alt={name}
+                          className="character-image"
+                        />
+                        <div>
+                          Status:
+                          {status === "Alive" ? (
+                            <i className="far fa-grin-beam" />
+                          ) : status === "Dead" ? (
+                            <i className="far fa-dizzy" />
+                          ) : (
+                            <i className="far fa-question-circle" />
+                          )}
+                          | Gender:
+                          {gender === "Male" ? (
+                            <i className="fas fa-mars" />
+                          ) : gender === "Female" ? (
+                            <i className="fas fa-venus" />
+                          ) : gender === "Genderless" ? (
+                            <i className="fas fa-genderless" />
+                          ) : (
+                            <i className="fas fa-question" />
+                          )}
+                          Origin {origin.name}
+                        </div>
+                      </div>
+                    )
+                  )
                 : "Nothing found"}
               <button type="button" onClick={() => setPage(prev)}>
                 Prev
