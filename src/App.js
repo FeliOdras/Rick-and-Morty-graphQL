c72ddd7089d212f9af7d873fdd3fa6b3d912ddb7
FeliOdras <ariane.feldmann@odras.de>
2019-06-18 09:14:09 +0200

    Imported and implemented Chars component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import ApolloClient, { gql } from "apollo-boost";
 import { ApolloProvider, Query } from "react-apollo";
 
+import { Characters } from "./components/Characters";
+
 const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql/"
 });
@@ -23,12 +25,13 @@ const GET_RM_CHARACTERS = gql`
   }
 `;
 
-function App({ page }) {
+function App() {
   const [page, setPage] = useState(1);
 
   return (
     <ApolloProvider client={client}>
       <div className="App">
+        <Characters />
         {/* <Query query={GET_RM_CHARACTERS} variables={{ page }}>
           {({
             loading,
---
+++ src/components/Characters.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Characters extends Component {
+  render() {
+    return <div>Chars</div>;
+  }
+}
+
+export default Characters;
