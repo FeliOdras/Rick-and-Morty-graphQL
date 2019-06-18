0c6d4acc4e74feadad472725ef1cc4c057268d4c
FeliOdras <ariane.feldmann@odras.de>
2019-06-18 09:09:01 +0200

    Import of useState hpook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ApolloClient, { gql } from "apollo-boost";
 import { ApolloProvider, Query } from "react-apollo";
 
@@ -6,34 +6,34 @@ const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql/"
 });
 
-function App() {
+const GET_RM_CHARACTERS = gql`
+  query data($page: Int) {
+    characters(page: $page) {
+      info {
+        count
+        next
+        prev
+        pages
+      }
+      results {
+        name
+        id
+      }
+    }
+  }
+`;
+
+function App({ page }) {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <Query
-          query={gql`
-            {
-              characters(page: 1) {
-                info {
-                  count
-                  next
-                  prev
-                  pages
-                }
-                results {
-                  name
-                  id
-                }
-              }
-            }
-          `}
-        >
+        <Query query={GET_RM_CHARACTERS} variables={{ page }}>
           {({
             loading,
             error,
             data: { characters: { info, results } = {} }
           }) => {
-            console.log(loading, error, info, results);
+            // console.log(loading, error, info, results);
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error :(</p>;
 
