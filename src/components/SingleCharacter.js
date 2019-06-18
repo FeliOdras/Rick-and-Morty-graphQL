c674d29751fd0c0e4785272bd3a2d7592d278e9c
FeliOdras <ariane.feldmann@odras.de>
2019-06-18 11:24:24 +0200

    Added single chjar query

--- src/components/SingleCharacter.js
+++ src/components/SingleCharacter.js
@@ -1,3 +1,38 @@
 import { React } from "react";
 import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
+
+const SingleCharacterQuery = gql`
+    query(Character: String!){
+        characters(filter:{name: $character}){
+            info {
+              count
+              next
+              prev
+              pages
+            }
+            results {
+              name
+              id
+              image
+              gender
+              species
+              status
+              origin {
+                name
+              }
+            }
+          }
+        }
+`;
+
+const SingleCharacter = () => {
+  const [character, setCharacter] = useState("Rick");
+
+  return (
+    <div>
+      <input type="text" />
+      <Query variables={{ character }} query={SingleCharacterQuery} />
+    </div>
+  );
+};
