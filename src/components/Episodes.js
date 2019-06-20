67b1363714292167a8cdb46863dcac78eb823ac8
FeliOdras <ariane.feldmann@odras.de>
2019-06-20 13:13:55 +0200

    Installled react router

---
+++ src/components/Episodes.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Episodes extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Episodes</h2>
+      </div>
+    );
+  }
+}
