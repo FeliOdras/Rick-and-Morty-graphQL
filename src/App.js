118a8c9931f3cd96af4d1324afa076a9aaeaaba1
FeliOdras <ariane.feldmann@odras.de>
2019-06-20 13:31:18 +0200

    Impemented Router in App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 // import of Apollo stuff
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import { Switch, BrowserRouter } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import SingleCharacter from "./components/SingleCharacter";
-import { BrowserRouter } from "react-router-dom";
+import Episodes from "./components/Episodes";
 // Apollo client
 const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql/"
@@ -20,6 +20,10 @@ class App extends React.Component {
     return (
       <>
         <ApolloProvider client={client}>
+          <Switch>
+            <Route exact path="/" component={SingleCharacter} />
+            <Route exact path="/episodes" component={Episodes} />
+          </Switch>
           <div className="App">
             <SingleCharacter />
           </div>
