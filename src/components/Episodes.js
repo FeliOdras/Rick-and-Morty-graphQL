7110ff62d43c7b9ce70afe84c91f9354952ca465
FeliOdras <ariane.feldmann@odras.de>
2019-06-20 15:23:00 +0200

    Added map and output to Episides

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -3,6 +3,7 @@ import { gql } from "apollo-boost";
 import { Query } from "react-apollo";
 
 import NavigationBar from "./NavigationBar";
+import { paginationButton } from "../helpers/index";
 
 const EpisodesQuery = gql`
   query($page: Int!) {
@@ -17,6 +18,7 @@ const EpisodesQuery = gql`
         id
         name
         air_date
+        episode
       }
     }
   }
@@ -29,12 +31,6 @@ function Episodes() {
       <header className="header">
         <h1>Rick & Morty Episodes Base</h1>
         <NavigationBar />
-        {/* <input
-          type="text"
-          value={episode}
-          placeholder="Type episode title"
-          onChange={e => setEpisode(e.target.value)}
-        /> */}
       </header>
       <main>
         <Query variables={{ page }} query={EpisodesQuery}>
@@ -48,13 +44,55 @@ function Episodes() {
               } = {}
             } = {}
           }) => {
-            console.log(loading, error, results);
+            if (loading)
+              return (
+                <p className="button">
+                  Loading <i className="fas fa-spinner fa-spin" />{" "}
+                </p>
+              );
+            if (error)
+              return (
+                <p className="button">
+                  Error <i className="fas fa-exclamation-triangle" />
+                </p>
+              );
 
-            return <>OK</>;
+            next = next ? next : pages;
+            prev = prev ? prev : 1;
+
+            return (
+              <>
+                <main>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Aired on</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {results
+                        ? results.map(({ id, name, air_date }) => (
+                            <tr key={id}>
+                              <td>{id}</td>
+                              <td>{name}</td>
+                              <td>{air_date}</td>
+                            </tr>
+                          ))
+                        : '<p className="button">Nothing found</p>'}
+                    </tbody>
+                  </table>
+                  <section className="pagination">
+                    <div className="pagination-pages">
+                      {paginationButton(pages, setPage, page)}
+                    </div>
+                  </section>
+                </main>
+              </>
+            );
           }}
         </Query>
-
-        {/* <Query variables={{ page, episode }} query={EpisodeQuery} /> */}
       </main>
     </>
   );
